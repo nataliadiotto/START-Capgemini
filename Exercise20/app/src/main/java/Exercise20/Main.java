4fc0b9589c17282fed249313a279e8001ba4464f
Natália Diotto <diottonatalia@gmail.com>
2023-07-23 22:15:10 -0400

    First Commit

---
+++ Exercise20/app/src/main/java/Exercise20/Main.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Exercise20;
+
+/**
+ *
+ * @author Natalia
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        int manufactYear = 0;
+        double carPrice = 0;
+        
+        boolean repeat = true;
+        
+        while (repeat == true) {
+            
+            
+            
+        }
+           
+        
+    }
+    
+}
