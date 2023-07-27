648daaa1af057208b6e396b23300d7f3e7520f52
Natália Diotto <diottonatalia@gmail.com>
2023-07-27 19:20:10 -0400

    Implementing Main class

---
+++ Exercise04/app/src/main/java/Exercise04/Main.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Exercise04;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Natalia
+ */
+public class Main {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        
+        
+        
+        sc.close();
+        
+        
+        
+    }
+    
+}
