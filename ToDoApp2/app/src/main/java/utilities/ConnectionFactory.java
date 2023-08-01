e8532da8936b5a145a5edc5b7918222ffff38135
Natália Diotto <diottonatalia@gmail.com>
2023-08-01 09:15:20 -0400

    Connecting to the database

---
+++ ToDoApp2/app/src/main/java/utilities/ConnectionFactory.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package utilities;
+
+/**
+ *
+ * @author Natalia
+ */
+public class ConnectionFactory {
+    
+    public static final String DRIVER = "com.mysql.jdbc.Driver";
+    public static final String URL = "jdbc.mysql://localhost:3306/toDoApp";
+    public static final String USER = "root";
+    public static final String PASS = "";
+    
+    
+    
+}
