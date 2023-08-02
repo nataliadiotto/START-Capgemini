7a86a46944e8b31b46c95fc830dfc343f268ac19
Natália Diotto <diottonatalia@gmail.com>
2023-08-02 09:15:20 -0400

    Opening the connection to the database

--- ToDoApp2/app/src/main/java/utilities/ConnectionFactory.java
+++ ToDoApp2/app/src/main/java/utilities/ConnectionFactory.java
@@ -4,6 +4,9 @@
  */
 package utilities;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 /**
  *
  * @author Natalia
@@ -14,6 +17,15 @@ public class ConnectionFactory {
     public static final String URL = "jdbc.mysql://localhost:3306/toDoApp";
     public static final String USER = "root";
     public static final String PASS = "";
+    
+    public static Connection getConnection() {
+        try {
+            Class.forName(DRIVER);  //carregar o drive
+            return DriverManager.getConnection(URL, USER, PASS);
+        } catch (Exception ex) {
+            throw new RuntimeException ("Error connecting to database.", ex);
+        }
+    }
     
     
     
