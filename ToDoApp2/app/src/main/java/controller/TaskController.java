29e419bbc1e564db41bc47ae9aad3edde520e1c6
Natália Diotto <diottonatalia@gmail.com>
2023-08-12 19:07:22 -0300

    Implementing RemoveByID method

--- ToDoApp2/app/src/main/java/controller/TaskController.java
+++ ToDoApp2/app/src/main/java/controller/TaskController.java
@@ -6,8 +6,10 @@ package controller;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 import model.Task;
+import utilities.ConnectionFactory;
 
 /**
  *
@@ -23,11 +25,25 @@ public class TaskController {
         
     }
     
-    public void removeById (int taskId) {
+    public void removeById (int taskId) throws SQLException {
         String sql = "DELETE FROM tasks WHERE id = ?";
         
         Connection conn = null;
         PreparedStatement statement = null;
+        
+        try {
+            
+            conn = ConnectionFactory.getConnection(); //criei a conexão
+            statement = conn.prepareStatement(sql); //objeto que ajuda a preparar o comando sql que será executado no banco
+            statement.setInt(1, taskId); //quero setar um valor naquele sql: substituir primeiro parametro (?) pelo valor da tarefa 
+            statement.execute();
+            
+        }  catch (SQLException e) {
+            throw new SQLException("Error deleting task.");
+        }  finally {
+            ConnectionFactory.closeConnection(conn);
+            
+        }
     }
     
     public List<Task> getAll(int idProject) { //search all tasks using idProject
