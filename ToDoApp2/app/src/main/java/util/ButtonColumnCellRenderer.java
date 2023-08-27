6897a5f3ccc771a157b22937cb2fba8a4c75ea25
Natália Diotto <diottonatalia@gmail.com>
2023-08-27 14:08:13 -0300

    Creating ButtonCellRenderer class

---
+++ ToDoApp2/app/src/main/java/util/ButtonColumnCellRenderer.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package util;
+
+import java.awt.Component;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableCellRenderer;
+
+/**
+ *
+ * @author Natalia
+ */
+public class ButtonColumnCellRenderer extends DefaultTableCellRenderer {
+    
+    
+    @Override
+    public Component getTableCellRendererComponent(JTable table, Object value,
+                          boolean isSelected, boolean hasFocus, int row, int column) {
+        
+    }
+}
