dcc17992c27dccb630757c1f81703f6f7e63f471
Natália Diotto <diottonatalia@gmail.com>
2023-08-27 13:41:23 -0300

    Implementing Deadline Cell Renderer

--- ToDoApp2/app/src/main/java/util/DeadlineColumnCellRenderer.java
+++ ToDoApp2/app/src/main/java/util/DeadlineColumnCellRenderer.java
@@ -4,10 +4,13 @@
  */
 package util;
 
+import java.awt.Color;
 import java.awt.Component;
+import java.util.Date;
 import javax.swing.JLabel;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableCellRenderer;
+import model.Task;
 
 /**
  *
@@ -23,8 +26,16 @@ public class DeadlineColumnCellRenderer extends DefaultTableCellRenderer {
         
         label.setHorizontalAlignment(CENTER);
         
+        TaskTableModel taskModel = (TaskTableModel) table.getModel(); //
+        Task task = taskModel.getTasks().get(row);
         
-        return null;
+        if (task.getDeadline().after(new Date())) {
+            label.setBackground(Color.green);
+        } else {
+            label.setBackground(Color.red);
+        }
+                
+        return label;
     }
     
 }
