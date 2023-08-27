4a93f0d11d018f3e3527a09133a4218ae9d1e181
Natália Diotto <diottonatalia@gmail.com>
2023-08-27 14:22:33 -0300

    Implementing Cell Renderer for Editing and Deleting tasks

--- ToDoApp2/app/src/main/java/util/ButtonColumnCellRenderer.java
+++ ToDoApp2/app/src/main/java/util/ButtonColumnCellRenderer.java
@@ -5,6 +5,7 @@
 package util;
 
 import java.awt.Component;
+import javax.swing.JLabel;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableCellRenderer;
 
@@ -14,10 +15,28 @@ import javax.swing.table.DefaultTableCellRenderer;
  */
 public class ButtonColumnCellRenderer extends DefaultTableCellRenderer {
     
+    private String buttonType;
     
+    public ButtonColumnCellRenderer (String buttonType) {
+        this.buttonType = buttonType;
+    }
+
+    public String getButtonType() {
+        return buttonType;
+    }
+
+    public void setButtonType(String buttonType) {
+        this.buttonType = buttonType;
+    }
+    
+        
     @Override
     public Component getTableCellRendererComponent(JTable table, Object value,
                           boolean isSelected, boolean hasFocus, int row, int column) {
+        JLabel label = (JLabel) super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+        label.setHorizontalAlignment(CENTER);
+        label.setIcon(new javax.swing.ImageIcon(getClass().getResource("/" + buttonType + ".png")));
         
+        return label;
     }
 }
--- ToDoApp2/app/src/main/java/view/MainScreen.java
+++ ToDoApp2/app/src/main/java/view/MainScreen.java
@@ -14,6 +14,7 @@ import java.util.List;
 import javax.swing.DefaultListModel;
 import model.Project;
 import model.Task;
+import util.ButtonColumnCellRenderer;
 import util.DeadlineColumnCellRenderer;
 import util.TaskTableModel;
 
@@ -494,8 +495,13 @@ public class MainScreen extends javax.swing.JFrame {
         //Criando ordenador automático das colunas da tabela (ordem alfabética)
         jTableTasks.setAutoCreateRowSorter(true);
         
-        //setando o cellrenderer que implementei
+        //setando o cellrenderer que implementei para coluna deadline
         jTableTasks.getColumnModel().getColumn(2).setCellRenderer(new DeadlineColumnCellRenderer());
+        
+        //setando o cellrenderer que implementei para a coluna editar e excluir
+        jTableTasks.getColumnModel().getColumn(4).setCellRenderer(new ButtonColumnCellRenderer("edit"));
+        jTableTasks.getColumnModel().getColumn(5).setCellRenderer(new ButtonColumnCellRenderer("delete"));
+        
     }
     
     public void initDataController() {
